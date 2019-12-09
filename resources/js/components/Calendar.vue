b0277fdd3384ad3f908c32322361be0d57cd284c
Joshua San Jose <josh@leandevinc.com>
2019-12-10 01:32:34 +0800

    add calendar and fix styles

---
+++ resources/js/components/Calendar.vue
@@ -0,0 +1,12 @@
+  
+<template>
+  <v-sheet height="500" elevation="5">
+    <v-calendar show-month-on-first type="month" value="startDate"></v-calendar>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "Calendar"
+};
+</script>
