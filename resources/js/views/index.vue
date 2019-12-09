3ab9c2b7a942612de95283aa2920683c186ffa39
Joshua San Jose <josh@leandevinc.com>
2019-12-10 01:55:11 +0800

    fix layout

--- resources/js/views/index.vue
+++ resources/js/views/index.vue
@@ -1,14 +1,28 @@
 <template>
   <v-container>
     <v-row>
-      <v-col sm="12" md="7" lg="7" xl="7"></v-col>
-      <v-col sm="12" md="5" lg="5" xl="5"></v-col>
+      <v-col sm="12" md="7" lg="7" xl="7">
+        <search-field />
+        <calendar />
+      </v-col>
+      <v-col sm="12" md="5" lg="5" xl="5">
+        <weather-card />
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import SearchField from "../components/SearchField";
+import WeatherCard from "../components/WeatherCard";
+import Calendar from "../components/Calendar";
+
 export default {
-  name: "Index"
+  name: "Index",
+  components: {
+    SearchField,
+    WeatherCard,
+    Calendar
+  }
 };
 </script>
