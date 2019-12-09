39442618acb00162961ec7f6cd299746a372089f
Joshua San Jose <josh@leandevinc.com>
2019-12-10 01:50:01 +0800

    add search field

---
+++ resources/js/components/SearchField.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet class="mb-6" elevation="5">
+    <v-autocomplete
+      solo
+      hide-details
+      hide-selected
+      placeholder="Search Cities"
+      :items="cities"
+      :menu-props="{ transition: true }"
+    ></v-autocomplete>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "SearchField",
+  data() {
+    return {
+      cities: ["Tokyo", "Yokohama", "Kyoto", "Osaka", "Sapporo", "Nagoya"]
+    };
+  },
+  methods: {}
+};
+</script>
