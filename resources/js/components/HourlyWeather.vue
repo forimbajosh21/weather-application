9ece3fea912ffe96c54a489da57173babdb72059
Joshua San Jose <josh@leandevinc.com>
2019-12-10 10:25:32 +0800

    add hours to hourly weather widget

--- resources/js/components/HourlyWeather.vue
+++ resources/js/components/HourlyWeather.vue
@@ -1,134 +1,55 @@
 <template>
   <v-row justify="center" align="center">
-    <v-divider vertical></v-divider>
-    <v-col sm="12" md="2" class="text-center">
-      <v-list-item-avatar
-        height="100%"
-        min-width="80"
-        width="80"
-        class="d-flex flex-column mx-auto"
-      >
-        <v-img
-          :src="'http://openweathermap.org/img/wn/' + currentData[0].weather[0].icon + '@2x.png'"
-          alt="Weather Logo"
-        />
+    <template v-for="(item, index) in [0, 2, 4, 6]">
+      <v-divider vertical :key="{index}"></v-divider>
+      <v-col sm="12" md="2" class="text-center" :key="{index}">
+        <v-list-item-avatar
+          height="100%"
+          min-width="80"
+          width="80"
+          class="d-flex flex-column mx-auto"
+        >
+          <v-img
+            :src="'http://openweathermap.org/img/wn/' + currentData[item].weather[0].icon + '@2x.png'"
+            alt="Weather Logo"
+          />
 
-        <span class="white--text caption description">
-          <PoseTransition appear>
-            <Box :i="300" v-if="isDisplay">{{currentData[0].weather[0].description}}</Box>
-          </PoseTransition>
-        </span>
-      </v-list-item-avatar>
-      <p class="white--text mb-1 caption">
-        High:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[0].main.temp_max)}}° C | {{toFarenheight(currentData[0].main.temp_max)}}° F</span>
-      </p>
-      <p class="white--text mb-0 caption">
-        Low:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[0].main.temp_min)}}° C | {{toFarenheight(currentData[0].main.temp_min)}}° F</span>
-      </p>
-    </v-col>
-    <v-divider vertical></v-divider>
-    <v-col sm="12" md="2" class="text-center">
-      <v-list-item-avatar
-        height="100%"
-        min-width="80"
-        width="80"
-        class="d-flex flex-column mx-auto"
-      >
-        <v-img
-          :src="'http://openweathermap.org/img/wn/' + currentData[2].weather[0].icon + '@2x.png'"
-          alt="Weather Logo"
-        />
-
-        <span class="white--text caption description">
-          <PoseTransition appear>
-            <Box :i="400" v-if="isDisplay">{{currentData[2].weather[0].description}}</Box>
-          </PoseTransition>
-        </span>
-      </v-list-item-avatar>
-      <p class="white--text mb-1 caption">
-        High:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[2].main.temp_max)}}° C | {{toFarenheight(currentData[2].main.temp_max)}}° F</span>
-      </p>
-      <p class="white--text mb-0 caption">
-        Low:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[2].main.temp_min)}}° C | {{toFarenheight(currentData[2].main.temp_min)}}° F</span>
-      </p>
-    </v-col>
-    <v-divider vertical></v-divider>
-    <v-col sm="12" md="2" class="text-center">
-      <v-list-item-avatar
-        height="100%"
-        min-width="80"
-        width="80"
-        class="d-flex flex-column mx-auto"
-      >
-        <v-img
-          :src="'http://openweathermap.org/img/wn/' + currentData[4].weather[0].icon + '@2x.png'"
-          alt="Weather Logo"
-        />
-
-        <span class="white--text caption description">
-          <PoseTransition appear>
-            <Box :i="500" v-if="isDisplay">{{currentData[4].weather[0].description}}</Box>
-          </PoseTransition>
-        </span>
-      </v-list-item-avatar>
-      <p class="white--text mb-1 caption">
-        High:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[4].main.temp_max)}}° C | {{toFarenheight(currentData[4].main.temp_max)}}° F</span>
-      </p>
-      <p class="white--text mb-0 caption">
-        Low:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[4].main.temp_min)}}° C | {{toFarenheight(currentData[4].main.temp_min)}}° F</span>
-      </p>
-    </v-col>
-    <v-divider vertical></v-divider>
-    <v-col sm="12" md="2" class="text-center">
-      <v-list-item-avatar
-        height="100%"
-        min-width="80"
-        width="80"
-        class="d-flex flex-column mx-auto"
-      >
-        <v-img
-          :src="'http://openweathermap.org/img/wn/' + currentData[6].weather[0].icon + '@2x.png'"
-          alt="Weather Logo"
-        />
-
-        <span class="white--text caption description">
-          <PoseTransition appear>
-            <Box :i="600" v-if="isDisplay">{{currentData[6].weather[0].description}}</Box>
-          </PoseTransition>
-        </span>
-      </v-list-item-avatar>
-      <p class="white--text mb-1 caption">
-        High:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[6].main.temp_max)}}° C | {{toFarenheight(currentData[6].main.temp_max)}}° F</span>
-      </p>
-      <p class="white--text mb-0 caption">
-        Low:
-        <span
-          class="font-weight-bold"
-        >{{toCelsius(currentData[6].main.temp_min)}}° C | {{toFarenheight(currentData[6].main.temp_min)}}° F</span>
-      </p>
-    </v-col>
-    <v-divider vertical></v-divider>
+          <span class="white--text caption description">
+            <PoseTransition appear>
+              <Box :i="300" v-if="isDisplay">{{currentData[item].weather[0].description}}</Box>
+            </PoseTransition>
+          </span>
+        </v-list-item-avatar>
+        <PoseTransition appear>
+          <Box :i="400" v-if="isDisplay">
+            <p class="white--text mb-1 caption">
+              <span class="font-weight-bold">{{currentData[item].dt_txt | moment('hh:mm a')}}</span>
+            </p>
+          </Box>
+        </PoseTransition>
+        <PoseTransition appear>
+          <Box :i="500" v-if="isDisplay">
+            <p class="white--text mb-1 caption">
+              High:
+              <span
+                class="font-weight-bold"
+              >{{toCelsius(currentData[item].main.temp_max)}}° C | {{toFarenheight(currentData[item].main.temp_max)}}° F</span>
+            </p>
+          </Box>
+        </PoseTransition>
+        <PoseTransition appear>
+          <Box :i="600" v-if="isDisplay">
+            <p class="white--text mb-0 caption">
+              Low:
+              <span
+                class="font-weight-bold"
+              >{{toCelsius(currentData[item].main.temp_min)}}° C | {{toFarenheight(currentData[item].main.temp_min)}}° F</span>
+            </p>
+          </Box>
+        </PoseTransition>
+      </v-col>
+      <v-divider vertical :key="{index}"></v-divider>
+    </template>
   </v-row>
 </template>
 
